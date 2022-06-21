e6492716ca580852f601e2c5dc1e59039820754a
KELVINMBAKA <loverc832@gmail.com>
2022-06-21 16:52:46 +0300

    commit tree a7a3b34285928d191af248a1e5ed515bbbbfcef8

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  // iterate from the beginning to the middle
+  for (let i = 0; i < word.length / 2; i++) {
+    // check each letter to the correponding letter from the end
+    const j = word.length - 1 - i;
+    // if any letters don't match, return false
+    if (word[i] !== word[j]) return false;
+  }
+
+  // return true
+  return true;
 }
 
-/* 
-  Add your pseudocode here
-*/
 
-/*
-  Add written explanation of your solution here
-*/
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
@@ -20,6 +23,11 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
 }
 
 module.exports = isPalindrome;
